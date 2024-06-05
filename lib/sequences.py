0d171d97d3418604a732c599d2f7006deba16de7
nunjayi <epenyash@gmail.com>
2024-06-05 11:04:54 +0300

    fibonacci function to print the fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    fib_sequence = [0, 1]
+    if length == 1:
+        print(fib_sequence[0])
+    
+    while len(fib_sequence) < length:
+        nxt_fib = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(nxt_fib)
+
+    print(fib_sequence)
+
+# Example:
+print_fibonacci(9)
